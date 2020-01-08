9ee2346f60f8d4b94f479b5ffbcce333066dff6d
Julian-Murillo <jomsk822@gmail.com>
2020-01-08 16:51:36 -0500

    refactored project structure and moved views into seperate folders in templates, added layouts file to templates and moved common html code to be reused using thymeleaf fragments

--- src/main/java/com/example/pma/controllers/HomeController.java
+++ src/main/java/com/example/pma/controllers/HomeController.java
@@ -28,6 +28,6 @@ public class HomeController {
         List<Employee> employees =  empRepo.findAll();
         model.addAttribute("employees", employees);
 
-        return "home";
+        return "main/home";
     }
 }
